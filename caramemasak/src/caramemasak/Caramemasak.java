f15b6209f3fa4ed1a953cfdfabcf12555f211ca6
jihargifari05 <66895507+jihargifari05@users.noreply.github.com>
2020-07-03 14:03:21 +0700

    upload

---
+++ caramemasak/src/caramemasak/Caramemasak.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caramemasak;
+
+
+/**
+ *
+ * @author hp
+ */
+public class Caramemasak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("cara memasak");
+       
+        //langkah 1
+        System.out.println("mengambi bumbu");
+        
+        //langkah 2
+        System.out.println("meracik bumbu");
+        
+        //langkah 3
+        System.out.println("mengolah masakan");
+        
+        //langkah 4
+        System.out.println("apakah garam terasa");
+        
+        boolean terasa = true;
+        if (terasa = true){
+            System.out.println("makanan siap dihidangkan");
+        }
+        else {
+            System.out.println("mengolah masakan lagi");
+        }
+    }
+    
+}
